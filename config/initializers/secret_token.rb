9796ca147edefeeea8ed6a10ff6a6585fc763234
captconrado <ottey001@gmail.com>
2013-03-15 14:23:16 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataVis::Application.config.secret_token = '2e44b52760cb140a5d118f8eeef4ac0f6f23fd26add1df70affa68a52edb9558e4ad2d61bf8fa19771e1247bcdee6dda6c8ac5a1c81da2094c0f041ae088e507'
